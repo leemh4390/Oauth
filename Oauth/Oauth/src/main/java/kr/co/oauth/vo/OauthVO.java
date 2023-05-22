1b376543a71c023f6b86e879fd93a37e8b787137
leemh44390 <leemh44390@gmail.com>
2023-05-22 16:49:17 +0900

    네이버 로그인 테스트

---
+++ Oauth/Oauth/src/main/java/kr/co/oauth/vo/OauthVO.java
@@ -0,0 +1,16 @@
+package kr.co.oauth.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OauthVO {
+	
+	private String uid;
+	private String pass;
+	private String username;
+	private String email;
+	private int count;
+
+}
---
+++ Oauth/Oauth/src/main/java/kr/co/oauth/vo/SnsInfoVO.java
@@ -0,0 +1,16 @@
+package kr.co.oauth.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SnsInfoVO {
+	
+	private int id;
+	private String sns_id;
+	private String sns_type;
+	private String sns_name;
+	private String sns_profile;
+	private String sns_connect_;
+}
